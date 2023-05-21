7062a5b582717a9323262babd8d2a22a9825d5dd
supriatnawebdev <yusupsupriatna442@gmailc.om>
2023-05-21 23:00:37 +0700

    belajar interpolation

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-hero',
   templateUrl: './hero.component.html',
-  styleUrls: ['./hero.component.css']
+  styleUrls: ['./hero.component.css'],
 })
 export class HeroComponent {
-
+  name = 'Yusup Supriatna';
+  cita_cita = 'programmer';
+  hobbies = ['ngoding', 'futsal', 'membantu sesama'];
+  sekolah = 'SMK';
+  getInfo(pendidikan = this.sekolah) {
+    return `Saya Lulusan: ${pendidikan}`;
+  }
 }
